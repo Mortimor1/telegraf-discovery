87d7d38bcd2073ed41542ffcf6d0756e25d22b16
maks <mortimor88@gmail.com>
2021-11-11 15:41:30 +0300

    First Commit

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/Mortimor1/telegraf-discovery
 
 go 1.14
+
+require (
+	github.com/tatsushid/go-fastping v0.0.0-20160109021039-d7bb493dee3e // indirect
+	golang.org/x/net v0.0.0-20211111083644-e5c967477495 // indirect
+	golang.org/x/sys v0.0.0-20211110154304-99a53858aa08 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/tatsushid/go-fastping"
+	"net"
+	"time"
+)
 
+func main() {
+	p := fastping.NewPinger()
+
+	ips, err := Hosts("10.10.0.0/23")
+	for _, ip := range ips {
+		p.AddIP(ip)
+	}
+
+	p.OnRecv = func(addr *net.IPAddr, rtt time.Duration) {
+		fmt.Printf("IP Addr: %s receive, RTT: %v\n", addr.String(), rtt)
+	}
+	p.OnIdle = func() {
+		fmt.Println("finish")
+	}
+	err = p.Run()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func Hosts(cidr string) ([]string, error) {
+	ip, subnet, err := net.ParseCIDR(cidr)
+	if err != nil {
+		return nil, err
+	}
+
+	var ips []string
+	for ip := ip.Mask(subnet.Mask); subnet.Contains(ip); inc(ip) {
+		ips = append(ips, ip.String())
+	}
+	// remove network address and broadcast address
+	return ips[1 : len(ips)-1], nil
+}
+
+func inc(ip net.IP) {
+	for j := len(ip) - 1; j >= 0; j-- {
+		ip[j]++
+		if ip[j] > 0 {
+			break
+		}
+	}
 }
